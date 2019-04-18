2dccc0f31b9b3b0c4e001010da437ab9f7750eb7
hyekyo <xiongmao1699@126.com>
2019-04-18 17:03:57 +0800

    no change

--- src/main/java/xiaofan/zhang/Test3.java
+++ src/main/java/xiaofan/zhang/Test3.java
@@ -13,11 +13,9 @@ public class Test3 {
         final 简繁转换类 简体转换器 = 简繁转换类.取实例(简繁转换类.目标.简体);
         String s = 简体转换器.转换("簡單");
         System.out.println(s);
-        String simplifiedStr = ZHConverter.convert("有背光的機械式鍵盤",ZHConverter.TRADITIONAL);
+        String simplifiedStr = ZHConverter.convert("有背光的機械式鍵盤",ZHConverter.SIMPLIFIED);
         System.out.println(simplifiedStr);
         String simplifiedStr2 = ZHConverter.convert("有背光的机械式键盘",ZHConverter.TRADITIONAL);
         System.out.println(simplifiedStr2);
-
     }
-
 }
