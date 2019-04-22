30e84ce96a1b5e7bf25974b1703e351d36a09744
hyekyo <xiongmao1699@126.com>
2019-04-22 17:21:25 +0800

    add new code

--- src/main/java/xiaofan/zhang/Test3.java
+++ src/main/java/xiaofan/zhang/Test3.java
@@ -1,21 +1,69 @@
 package xiaofan.zhang;
 
-import com.github.nobodxbodon.zhconverter.简繁转换类;
-import com.spreada.utils.chinese.ZHConverter;
 
 /**
  * Created by zhangxiaofan on 2019/4/9.
  */
 public class Test3 {
-
     public static void main(String[] args) {
-        final 简繁转换类 繁体转换器 = 简繁转换类.取实例(简繁转换类.目标.繁体);
-        final 简繁转换类 简体转换器 = 简繁转换类.取实例(简繁转换类.目标.简体);
-        String s = 简体转换器.转换("簡單");
+        String str = "babcbabcbaccba";
+        str = "bacab";
+        String s = longestPalindrome1(str);
         System.out.println(s);
-        String simplifiedStr = ZHConverter.convert("有背光的機械式鍵盤",ZHConverter.SIMPLIFIED);
-        System.out.println(simplifiedStr);
-        String simplifiedStr2 = ZHConverter.convert("有背光的机械式键盘",ZHConverter.TRADITIONAL);
-        System.out.println(simplifiedStr2);
+        int maxlong=0;
+        String maxlongs="";
+        for (int i = 0; i <str.length() ; i++) {
+            for (int j = i+1; j <str.length() ; j++) {
+                int len = j-i;
+                String curr = str.substring(i,j+1);
+                if(ok(curr)){
+                    if(len>maxlong){
+                        maxlong=len;
+                        maxlongs=curr;
+                    }
+                }
+            }
+        }
+        System.out.println(maxlongs);
+
+    }
+    public static Boolean ok(String s){
+        for (int i = 0; i <s.length() ; i++) {
+            if (s.charAt(i)!=s.charAt(s.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static String longestPalindrome1(String s) {
+        int maxPalinLength = 0;
+        String longestPalindrome = null;
+        int length = s.length();
+
+        // check all possible sub strings
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int len = j - i;
+                String curr = s.substring(i, j + 1);
+                if (isPalindrome(curr)) {
+                    if (len > maxPalinLength) {
+                        longestPalindrome = curr;
+                        maxPalinLength = len;
+                    }
+                }
+            }
+        }
+
+        return longestPalindrome;
+    }
+
+    public static boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
