dfd57111366d4812102056cf780300345cff1509
hyekyo <xiongmao1699@126.com>
2019-06-03 14:31:55 +0800

    add new

---
+++ src/main/java/xiaofan/zhang/demo/demo1.java
@@ -0,0 +1,58 @@
+package xiaofan.zhang.demo;
+
+import xiaofan.zhang.other.App;
+
+import java.util.*;
+
+/**
+ * Created by zhangxiaofan on 2019/6/3.
+ */
+public class demo1 {
+
+    public static void main(String[] args) {
+        demo1 demo1 = new demo1();
+        int[] ints = {4,9,5};
+        int[] ints1 = {9,4,9,8,4};
+        int[] ints2 = demo1.intersect1(ints, ints1);
+        App.print(ints2);
+    }
+
+    public int[] intersect1(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>();
+        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i <nums1.length ; i++) {
+            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
+        }
+        for (int j = 0; j <nums2.length ; j++) {
+            if(map.containsKey(nums2[j])){
+                if(map.get(nums2[j])>0){
+                    list.add(nums2[j]);
+                    map.put(nums2[j],map.get(nums2[j])-1);
+                }
+            }
+        }
+        int[] ints = new int[list.size()];
+        for (int i = 0; i <list.size() ; i++) {
+            ints[i]=list.get(i);
+        }
+        return ints;
+    }
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i = 0, j = 0;
+        int [] ans = new int [nums2.length];
+        int index = 0;
+        while(i++ < nums1.length && j++ < nums2.length){
+            if(nums1[i - 1] == nums2[j - 1])
+                ans[index++] = nums1[i - 1];
+            else if(nums1[i - 1] < nums2[j - 1])
+                j--;
+            else
+                i--;
+        }
+        return Arrays.copyOf(ans, index);
+    }
+}
--- src/main/java/xiaofan/zhang/other/Demo.java
+++ src/main/java/xiaofan/zhang/other/Demo.java
@@ -1,5 +1,8 @@
 package xiaofan.zhang.other;
 
+
+import java.util.HashMap;
+
 /**
  * Created by zhangxiaofan on 2019/5/28.
  */
@@ -9,10 +12,138 @@ public class Demo {
         int[]  nums2 = {2,5,6};
         int m = 3;
         int n = 3;
-        merge(nums1,m,nums2,n);
-        int i = removeDuplicates4(new int[]{0,0,1,1,1,2,2,3,3,4});
-        int[] ints = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        //merge(nums1,m,nums2,n);
+        //int i = removeDuplicates4(new int[]{0,0,1,1,1,2,2,3,3,4});
+        int[] ints = {1,2,2,1};
+        int[] ints1 = {2,2};
+        int[] delete = delete(ints, 2);
+        App.print(delete);
     }
+    public static int[] delete(int[] arr,int index){
+        int[] newArr = new int[arr.length-1];
+        for (int i = 0; i < arr.length; i++) {
+            if(index>i){
+                newArr[i]=arr[i];
+            }else if(index<i){
+                newArr[i-1]=arr[i];
+            }
+        }
+        return newArr;
+    }
+
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        int len =0;
+        int index=0;
+        if(nums1.length>nums2.length){
+            len=nums2.length;
+        }else{
+            len=nums1.length;
+        }
+        int[] arr = new int[len];
+        if(nums1==null||nums2==null){
+            return new int[0];
+        }
+        for(int i =0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    arr[index]=nums1[i];
+                    index++;
+                    break;
+                }
+            }
+        }
+        int[] result = new int[index++];
+        for(int x=0;x<result.length;x++){
+            result[x]=arr[x];
+        }
+        return result;
+    }
+    public static int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i <nums.length ; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+
+    public static void rotate1(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        //    1,2,3,4,5,6,7
+        //    4,2,3,1,5,6,7
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+
+
+    public static void reverse(int[] nums,int start,int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
+    public static void rotate(int[] nums, int k) {
+        int len = nums.length;
+        int mid = (0+len-1)/2;
+        if(mid<k){
+            return;
+        }
+        for(int i=0 ;i<k;i++){
+            int tmp = nums[i];
+            nums[i] = nums[len-k+i];
+            nums[len-k+i] = tmp;
+        }
+    }
+
+    public static int maxProfit1(int[] prices) {
+        int max = 0;
+        int size = prices.length;
+        for (int i = 0; i < size - 1; ++i){
+            if (prices[i] < prices[i + 1]){
+                max += prices[i + 1] - prices[i];
+            }
+        }
+        return max;
+    }
+
+
+    public int maxProfit(int[] prices) {
+        int sum=0;
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i =0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            }
+            else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+                sum+=maxprofit;
+                minprice = Integer.MAX_VALUE;
+                maxprofit = 0;
+                i--;
+            }
+        }
+
+
+        return sum;
+    }
+
     public static int removeDuplicates4(int[] nums){
         int count=0;
         // 0,1,1,1,2,2,3,3,4
